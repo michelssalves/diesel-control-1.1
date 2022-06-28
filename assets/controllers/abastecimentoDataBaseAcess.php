8aae82825ecf9d8ce0d7ebcfe23709bb087c963f
Michel <michelssalves@gmail.com>
2022-06-28 11:29:10 -0300

    2022-06-28 11:16

--- assets/controllers/abastecimentoDataBaseAcess.php
+++ assets/controllers/abastecimentoDataBaseAcess.php
@@ -303,6 +303,7 @@ function listarAbastecimentos(){
                     $corLitros = '';
                         if($row['media'] < 1.5){$corMedia = 'bg-danger';}
                         if($row['media'] > 2.5 && $row['descricao_caminhao'] == 'COMPACTADOR'){$corMedia = 'bg-warning';}
+                        if($row['media'] > 15 ){$corMedia = 'bg-danger';}
                          if($row['litros_od'] <> $row['litros'] ){$corLitros = 'bg-warning';}
     
                     $txtTable = $txtTable.'<tr>
