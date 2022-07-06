e30e2b17bf767cc9fd31559d9cc807144cdce4cd
Michel <michelssalves@gmail.com>
2022-07-06 15:43:50 -0300

    2022-07-06 15:24

--- assets/controllers/abastecimentoDataBaseAcess.php
+++ assets/controllers/abastecimentoDataBaseAcess.php
@@ -438,7 +438,7 @@ function listarAcertos($id_funcionario){
         $txtTableQuadro = $txtTableQuadro.'<tr>
         <td><center>'.$row['acertos'].'</td>
         <td><center>'.$row['erros'].'</td>
-        <td><center>'.number_format($v2,'2',',','.').'</td>
+        <td><center>'.number_format($v2,'2',',','.').'%</td>
         </tr>';
         
     }
