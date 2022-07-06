40a85070052dff0794df78b887f81181629e437b
Michel <michelssalves@gmail.com>
2022-07-06 10:09:11 -0300

    2022-07-06 10:09

--- assets/controllers/abastecimentoDataBaseAcess.php
+++ assets/controllers/abastecimentoDataBaseAcess.php
@@ -192,19 +192,6 @@ function registrarAbastecimento(){
     $data_abastecimento = dmaHLocal($_POST['data_abastecimento']);
     $data_sem_hora = Ymd($data_abastecimento);
 
-    if($diferencakm < 0 || $diferencakm > 600){
-        $id_erro = 1;
-        registrarErro($id_funcionario, $id_erro);
-    }
-    if($diferencahr < 0 || $diferencahr > 48){
-        $id_erro = 2;
-        registrarErro($id_funcionario, $id_erro);
-    }
-    if($litros <> $litros_od){
-        $id_erro = 3;
-        registrarErro($id_funcionario, $id_erro);
-    }
-
     if($data_abastecimento == ''){
          
         $data_abastecimento = new DateTime('NOW', new DateTimeZone('America/Sao_Paulo'));
@@ -378,22 +365,6 @@ function consultarIdEquipamento($numero_equipamento){
     return  $id_veiculo;
  
 }
-function registrarErro($id_funcionario, $id_erro){
 
-    include 'config.php';
-    include 'functions.php';
-
-    $erro_data = new DateTime('NOW', new DateTimeZone('America/Sao_Paulo'));
-    $erro_data = $erro_data->format('Y-m-d H:i');
-    $erro_status = 1;
-    
-    $sql = $pdo->prepare("INSERT INTO erros_de_registro(id_funcionario, id_erro, erro_status, erro_data) VALUES(:id_funcionario, id_erro, erro_status, erro_data)");
-    $sql->bindValue(':id_funcionario', $id_funcionario);
-    $sql->bindValue(':erro_km', $id_erro);
-    $sql->bindValue(':erro_status', $erro_status);
-    $sql->bindValue(':erro_data', $erro_data);
-    $sql->execute();
-
-}
 
 ?>
