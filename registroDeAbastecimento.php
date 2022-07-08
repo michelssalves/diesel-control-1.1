243b2af8d1fcb160cff7e8b50bd7232d93f43f48
unknown <michelssalves@gmail.com>
2022-07-07 22:16:14 -0300

    2022-07-07 22:10

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -29,8 +29,10 @@ include 'assets/controllers/checkAcess.php';
                 <div class="container-xxl">
                 <div class="w3-bar w3-light-grey">
                     <a href="logout"class="w3-bar-item w3-button w3-red w3-right">Sair</a>
-                    <label class="w3-bar-item w3-button w3-left">Usuario Logado:</label>
+                    <a href="menu-principal" class="w3-bar-item w3-button w3-blue w3-right">Voltar</a>
+                    
                     <a class="w3-bar-item w3-button"><?= $usuario; ?></a>
+                    <label class="w3-bar-item w3-button w3-left">Usuario Logado:</label>
                 </div> 
                 <div class="w3-bar w3-light-grey w3-container">
                 <table class="table table-bordered" style="border: 10px;">
