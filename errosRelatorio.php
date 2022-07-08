cdb42dc3ca757830d732dc8c96a8c34f3837691c
unknown <michelssalves@gmail.com>
2022-07-08 00:02:24 -0300

    2022-07-07 22:58

--- errosRelatorio.php
+++ errosRelatorio.php
@@ -16,6 +16,8 @@ $sql = $pdo->prepare("SELECT erro_status FROM erros_de_registro WHERE id = :idEr
 $sql->bindValue(':idErro', $idErro);
 $sql->execute();
 $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+print_r($lista);
+var_dump($lista);
 echo $erro_status = $lista['erro_status'];
 ?>
 <!doctype html>
