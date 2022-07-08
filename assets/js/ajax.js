0f7f60d51e12c7b605f8cdb4adb63de1afbefbb2
Michel <michelssalves@gmail.com>
2022-07-08 08:33:48 -0300

    2022-07-08 08:233

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -15,7 +15,7 @@ async function alterarStatusErro(idErro, id_abastecimento){
 	const idRow =`td${id_abastecimento}`
 
 	novoStatus = json.erro_status
-	alert()
+	alert(novoStatus)
 	if(novoStatus == 1){
 		document.getElementById(idRow).innerHTML='N�O'
 		document.getElementById(idRow).className='w3-red'
