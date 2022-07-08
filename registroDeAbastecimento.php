18b5115ff544a2879c53f66154fcb5ea504d4c15
unknown <michelssalves@gmail.com>
2022-07-07 22:32:56 -0300

    2022-07-07 21-50

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -36,7 +36,7 @@ include 'assets/controllers/checkAcess.php';
                 <table class="table table-bordered" style="border: 10px;">
                 <thead class="thead-dark" >
                     <tr>
-                    <td colspan="3"><center><strong>Clique em erros para visualizar seu histórico</strong></td>
+                    <td colspan="3" style="font-weight:bold"><center>CLIQUE EM ERROS PARA VER SEU HISTORICO</td>
                     </tr>
                     <tr>
                         <th class="text-success"><center>Acertos</th>
