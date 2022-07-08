c80d5f8b436e5733775b86d71a2ebf1ad1a35269
Michel <michelssalves@gmail.com>
2022-07-08 08:35:19 -0300

    2022-07-08 08:233

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -84,3 +84,30 @@ function table2excel(id) {
 if ( window.history.replaceState ) {
     window.history.replaceState( null, null, window.location.href );
 }
+async function alterarStatusErro(idErro, id_abastecimento){
+    const acao = 'alterar-status-erro'
+	const data = new FormData()
+	data.append('idErro', idErro)
+    data.append('id_abastecimento', id_abastecimento)
+    data.append('acao', acao)
+
+	const req = await fetch('diesel-control-1.1/assets/controllers/abastecimentoDataBaseAcess.php', {
+		method: 'POST',
+		body: data
+	})
+
+	const json = await req.json()
+
+	const idRow =`td${id_abastecimento}`
+
+	novoStatus = json.erro_status
+
+	alert(novoStatus)
+	if(novoStatus == 1){
+		document.getElementById(idRow).innerHTML='NÃO'
+		document.getElementById(idRow).className='w3-red'
+	}else{
+		document.getElementById(idRow).innerHTML='SIM'
+		document.getElementById(idRow).className='w3-green'
+	}
+}
