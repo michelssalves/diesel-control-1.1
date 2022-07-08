fd58dbcd9e88ad4d29e63270de53fabd8c4056ba
unknown <michelssalves@gmail.com>
2022-07-07 23:14:43 -0300

    2022-07-07 22:58

--- assets/controllers/abastecimentoDataBaseAcess.php
+++ assets/controllers/abastecimentoDataBaseAcess.php
@@ -52,7 +52,7 @@ if($setor && $setor <> 'TODOS'){$filtroSetor = "AND v.setor = '$setor'";}
 if($dataIncial  == ''){
 
     $x = new DateTime('NOW', new DateTimeZone('America/Sao_Paulo'));
-    $dataHoraIncial = $x->format('d-m-Y');
+    $dataHoraIncial = $x->format('Y-m-d 00:00');
 
     //$dataIncial = date('Y-m-d');
     //$dataHoraIncial = date('Y-m-d 00:00');
@@ -63,7 +63,7 @@ if($dataIncial  == ''){
 if($dataFinal == ''){ 
 
     $x = new DateTime('NOW', new DateTimeZone('America/Sao_Paulo'));
-    $dataHoraFinal = $x->format('d-m-Y');
+    $dataHoraFinal = $x->format('Y-m-d 23:59');
 
    //$dataHoraFinal = date('Y-m-d 23:59');
     //$dataFinal = date('Y-m-d'); 
