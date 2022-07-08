02da704c3eb1f4c305826029faca47f04a660dcf
unknown <michelssalves@gmail.com>
2022-07-08 00:36:20 -0300

    2022-07-07 22:58

---
+++ assets/js/ajax.js
@@ -0,0 +1,24 @@
+async function alterarStatusErro(idErro, id_abastecimento){
+    const acao = 'alterar-status-erro'
+	const data = new FormData()
+	data.append('idErro', idErro)
+    data.append('id_abastecimento', id_abastecimento)
+    data.append('acao', acao)
+
+	const req = await fetch('diesel-control-1.1/assets/controllers/abastecimentoDataBaseAcess.php', {
+		method: 'POST',
+		body: data
+	})
+
+	const json = await req.json()
+
+	novoStatus = json.erro_status
+
+	if(novoStatus == 0){
+		document.getElementById(id_abastecimento).innerHTML='NÃO'
+		document.getElementById(id_abastecimento).className='w3-red'
+	}else{
+		document.getElementById(id_abastecimento).innerHTML='SIM'
+		document.getElementById(id_abastecimento).className='w3-green'
+	}
+}
