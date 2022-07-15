329d517601f6124a510305cd8dbc4d26534f1a48
Michel <michelssalves@gmail.com>
2022-07-15 17:28:32 -0300

    2022-07-15 09:41

--- assets/controllers/abastecimentoDataBaseAcess.php
+++ assets/controllers/abastecimentoDataBaseAcess.php
@@ -312,7 +312,8 @@ if($media > 0){
     $_SESSION['msg'] = '<div class="w3-green">CADASTRADO COM SUCESSO!</div>';
     header("Location: abastecer-veiculos");
     }else{
-        $_SESSION['msg'] = '<div class="w3-red">MÉDIA INCORRETA!</div>';
+        $_SESSION['msg'] = '<div class="w3-red">NÃO REGISTRADO! MÉDIA INCORRETA!</div>';
+        header("Location: abastecer-veiculos");
     }
 } 
 function alterarAbastecimento(){
