10742ea7c7a5f3ea7b3f0f39cb6065b5eff8ef19
Michel <michelssalves@gmail.com>
2022-09-01 11:45:20 -0300

    cadastrar veiculo select

--- veiculos.php
+++ veiculos.php
@@ -19,7 +19,6 @@ include 'assets/controllers/checkAcess.php';
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="diesel-control-1.1/assets/css/menucontroles.css">
     <script src="diesel-control-1.1/assets/js/sorttable.js"></script>
     <script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>
 </head>
