c6112a91dd55698e1bcc819d2f80d8de88e094b6
Michel <michelssalves@gmail.com>
2022-12-26 12:46:59 -0300

    ajustando json Ref: 26-12-2022 - 12:46 -> Michel

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -59,7 +59,7 @@ include 'assets/controllers/checkAcess.php';
                         <div class="field">
                             <div class="control">
                                 <label>Prefixo</label>
-                                <select class="form-select" name="prefixo" id="prefixo" onchange="buscarInfoVeiculo()" required>
+                                <select class="form-select" name="prefixo" id="prefixo" onchange="buscarInfoVeiculo(this.id)" required>
                                     <option value="">Escolha o Prefixo</option>
                                     <?php
                                     $ativado = 1;
