97a158bbfcaf92e73fbeb1329f66a751affcfff0
Michel <michelssalves@gmail.com>
2022-12-26 16:25:42 -0300

    teste Ref: 26-12-2022 - 16:25 -> Michel

--- controles.php
+++ controles.php
@@ -10,24 +10,24 @@ $id_funcionario = $_SESSION['id_funcionario'];
 $token = $_SESSION['token'];
 include 'assets/controllers/checkAcess.php';
 ?>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/custom.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/fontawesome.all.min.6.2.1.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/tablesaw.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/w3.min.css">
+    <script src="diesel-control-1.1/assets/js/fontawesome.all.min.js"></script>
+    <script src="diesel-control-1.1/assets/js/bootstrap.bundle.min.v5.2.3.js"></script>
     <!--  TABLESAW - DEIXA A TABELA RESPONSIVA-->
     <script src="diesel-control-1.1/assets/js/tablesawn-label.js"></script>
     <script src="diesel-control-1.1/assets/js/tablesaw.js"></script>
     <script src="diesel-control-1.1/assets/js/tablesaw-init.js"></script>
-    <!--  /TABLESAW - DEIXA A TABELA RESPONSIVA-->
+   <!--  /TABLESAW - DEIXA A TABELA RESPONSIVA-->
     <script src="diesel-control-1.1/assets/js/jquery-3.6.1.min.js"></script>
     <script src="diesel-control-1.1/assets/js/sorttable.js"></script>
-    <script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>
+    <script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>  
 </head>
 <body>
     <div class="w3-bar w3-light-grey">
