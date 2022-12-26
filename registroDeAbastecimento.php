ab405842877b9a1ad5af217c26b2bcef90fc164c
Michel <michelssalves@gmail.com>
2022-12-26 11:01:37 -0300

    ajustado css Ref: 26-12-2022 - 11:01 -> Michel

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -16,11 +16,14 @@ include 'assets/controllers/checkAcess.php';
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+     <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/menuregistro.css">    
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> -->
+    <link rel="stylesheet" href="diesel-control-1.1/assets/css/custom.css">
+    <link rel="stylesheet" href="diesel-control-1.1/assets/css/fontawesome.all.min.6.2.1.css">
+    <link rel="stylesheet" href="diesel-control-1.1/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/tablesaw.css">
 </head>
 <body>
@@ -193,7 +196,9 @@ include 'assets/controllers/checkAcess.php';
     </div>
     </div>  
     <div class="container">
-        <div class="row">                 
+        <div class="row"> 
+            <div class="table-responsive"> 
+                <div class="tabelas-ver-cheque">             
             <table data-tablesaw-sortable data-tablesaw-sortable-switch class="tablesaw table-sm table-hover table-striped fs-6 mb-0" data-tablesaw-mode="columntoggle" data-tablesaw-minimap>
               <thead class="thead-dark">
                     <tr>
@@ -219,9 +224,14 @@ include 'assets/controllers/checkAcess.php';
                     <?= listarAbastecimentos()?>
                 </tbody>
             </table>
+            </div> 
+            </div> 
         </div>
     </div>
     <script src="diesel-control-1.1/assets/js/scripts.js"></script>
+    <script src="diesel-control-1.1/assets/js/fontawesome.all.min.js"></script>
+    <script src="diesel-control-1.1/assets/js/jquery-3.6.1.min.js"></script>
+    <script src="diesel-control-1.1/assets/js/bootstrap.bundle.min.v5.2.3.js"></script>
         <!-- TABLESAW - DEIXA A TABELA RESPONSIVA-->
     <script src="diesel-control-1.1/assets/js/tablesawn-label.js"></script>
     <script src="diesel-control-1.1/assets/js/tablesaw.js"></script>
