d8a1e003989766d365d978c8c00fbec84e479fc7
Michel <michelssalves@gmail.com>
2022-12-26 11:15:34 -0300

    ajustado prioridade Ref: 26-12-2022 - 11:15 -> Michel

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -203,22 +203,22 @@ include 'assets/controllers/checkAcess.php';
             <table data-tablesaw-sortable data-tablesaw-sortable-switch class="tablesaw table-sm table-hover table-striped fs-6 mb-0" data-tablesaw-mode="columntoggle" data-tablesaw-minimap>
               <thead class="thead-dark">
                     <tr>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Data</th>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="5"><center>Prefixo Sap</th>
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Data</th>
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Prefixo Sap</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Placa</th>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Prefixo</th>
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Prefixo</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>ODI</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>ODF</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Litros Od</th>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="5"><center>Litros</th>
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Litros</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Media</th>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="5"><center>Ultimo Km</th>
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Ultimo Km</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Km</th>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="5"><center>Dif Km</th>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="5"><center>Ultimo Hr</th>
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Dif Km</th>
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Ultimo Hr</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Hr</th>
                         <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Dif Hr</th>
-                        <th data-tablesaw-sortable-col data-tablesaw-priority="1"><center>Frentista</th>  
+                        <th data-tablesaw-sortable-col data-tablesaw-priority="3"><center>Frentista</th>  
                     </tr>
                 </thead>
                 <tbody>
