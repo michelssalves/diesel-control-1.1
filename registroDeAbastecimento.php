2180e70d8d128551b5cac4d38a4a1f37acdfbd4d
Michel <michelssalves@gmail.com>
2022-12-26 11:18:40 -0300

    ajustando tabela registro de abastecimento Ref: 26-12-2022 - 11:18 -> Michel

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -198,8 +198,8 @@ include 'assets/controllers/checkAcess.php';
     </div>  
     <div class="container">
         <div class="row"> 
-            <div class="table-responsive"> 
-                <div class="tabela-ver-cheque">             
+        <div class="table-responsive">
+         <div class="tabela-ver-todos-os-cheques">           
             <table data-tablesaw-sortable data-tablesaw-sortable-switch class="tablesaw table-sm table-hover table-striped fs-6 mb-0" data-tablesaw-mode="columntoggle" data-tablesaw-minimap>
               <thead class="thead-dark">
                     <tr>
