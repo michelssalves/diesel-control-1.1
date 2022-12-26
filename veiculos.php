43e50383d7241e30a6be6302f2914ccd7e176048
Michel <michelssalves@gmail.com>
2022-12-26 15:14:00 -0300

    ajustado jq Ref: 26-12-2022 - 15:13 -> Michel

--- veiculos.php
+++ veiculos.php
@@ -19,14 +19,15 @@ include 'assets/controllers/checkAcess.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="diesel-control-1.1/assets/js/jquery-3.6.1.min.js"></script>
 -->
-     <link rel="stylesheet" href="diesel-control-1.1/assets/css/custom.css">
+    <link rel="stylesheet" href="diesel-control-1.1/assets/css/custom.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/fontawesome.all.min.6.2.1.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/tablesaw.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/w3.min.css">
+   
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="diesel-control-1.1/assets/js/sorttable.js"></script>
-    <script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>
+    <script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>  
 </head>
 <body>
     <div class="w3-bar w3-light-grey">
