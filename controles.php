5de47926b66374c8613900ec73cda8c0dfdb5924
Michel <michelssalves@gmail.com>
2022-12-26 15:46:52 -0300

    removido erro Ref: 26-12-2022 - 15:46 -> Michel

--- controles.php
+++ controles.php
@@ -22,7 +22,7 @@ include 'assets/controllers/checkAcess.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="diesel-control-1.1/assets/js/sorttable.js"></script>
-	<script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>  -->$_COOKIE
+	<script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>  -->
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/custom.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/fontawesome.all.min.6.2.1.css">
     <link rel="stylesheet" href="diesel-control-1.1/assets/css/bootstrap.min.css">
