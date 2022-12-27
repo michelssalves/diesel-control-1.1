a666776762bce8e4b57dde8d2be48e4e87898828
Michel <michelssalves@gmail.com>
2022-12-27 14:47:37 -0300

    ajustado km Ref: 27-12-2022 - 14:47 -> Michel

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -93,7 +93,7 @@ include 'assets/controllers/checkAcess.php';
 
         <div class="input-group input-group-sm mb-3">
             <span class="input-group-text" id="inputGroup-sizing">Km:</span>
-            <input onkeyup="calcularMedia()" onkeypress="return soNumeros()" onkeyup="calcularDiferencaKm()" id="km" name="kmRegistrar"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Km" required>
+            <input onkeyup="calcularDiferencaKm(),calcularMedia()" onkeypress="return soNumeros()" id="km" name="kmRegistrar"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" placeholder="Km" required>
         </div>
 
         <div class="input-group input-group-sm mb-3">
