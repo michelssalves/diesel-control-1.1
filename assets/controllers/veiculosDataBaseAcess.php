c2ca84f1986ab15921d5601aa1739d8f164c3dfb
Michel <michelssalves@gmail.com>
2022-12-27 09:53:10 -0300

    resultados Ref: 27-12-2022 - 09:53 -> Michel

--- assets/controllers/veiculosDataBaseAcess.php
+++ assets/controllers/veiculosDataBaseAcess.php
@@ -198,20 +198,21 @@ function filtrarVeiculos($filtroPrefixo, $filtroCombustivel,$filtroMarca, $filtr
                 include 'modalAlterarVeiculos.php';
             }
         $txtTableVeiculos .='</tbody></table>';
-
+        
+        $resultados = $sql->rowCount();
         $txtTableVeiculos .= '<nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-center">
-    <li class="page-item disabled">
-      <a class="page-link">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>';
+                                    <ul class="pagination justify-content-center">
+                                        <li class="page-item disabled">
+                                        <a class="page-link">Previous</a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="#">'.$resultados.'</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item">
+                                        <a class="page-link" href="#">Next</a>
+                                        </li>
+                                    </ul>
+                                </nav>';
         
         }
           return  $txtTableVeiculos;      
