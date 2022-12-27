19fc314278855c01706014dd4d9460fcdb7f2c13
Michel <michelssalves@gmail.com>
2022-12-27 17:55:44 -0300

    teste Ref: 27-12-2022 - 17:55 -> Michel

--- assets/controllers/abastecimentoDataBaseAcess.php
+++ assets/controllers/abastecimentoDataBaseAcess.php
@@ -169,6 +169,7 @@ if($acao == 'alterar-abastecimento'){
     
 }
 if($acao == 'excluir-abastecimento'){
+    include 'config.php';
 
     $id_abastecimentoAlterar = $_POST['id_abastecimentoAlterar'];
     $sql = $pdo->prepare("DELETE FROM abastecimentos WHERE id_abastecimento = :id_abastecimentoAlterar");
