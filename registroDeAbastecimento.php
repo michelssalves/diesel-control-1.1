f7599ab6e690c0785db41a5241bbb25d626a306d
Michel <michelssalves@gmail.com>
2022-12-27 16:34:19 -0300

    teste Ref: 27-12-2022 - 16:34 -> Michel

--- registroDeAbastecimento.php
+++ registroDeAbastecimento.php
@@ -188,7 +188,7 @@ include 'assets/controllers/abastecimentoDataBaseAcess.php';
                         </tr>
                     </thead>
                     <tbody>
-                        <?= $txtTable ?>
+                        <?= listarAbastecimentos() ?>
                     </tbody>
                 </table>
                 </form> 
