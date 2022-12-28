02c5ec14dc69585e7fc18e2fbf652967743e9d06
Michel <michelssalves@gmail.com>
2022-12-28 17:28:02 -0300

    teste Ref: 28-12-2022 - 17:28 -> Michel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -53,58 +53,10 @@ function dividirConvertendo(v1, v2){
 	return mediaFinal = media.toFixed(2).toString().replace('.', ',')
 
 }
-
-function PopupCenter(url, title, w, h) {  
-    // Fixes dual-screen position                         Most browsers      Firefox  
-    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;  
-    var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;  
-              
-    width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;  
-    height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;  
-              
-    var left = ((width / 2) - (w / 2)) + dualScreenLeft;  
-    var top = ((height / 2) - (h / 2)) + dualScreenTop;  
-    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);  
-  
-    // Puts focus on the newWindow  
-    if (window.focus) {  
-        newWindow.focus();  
-    }  
-}  
-if ( window.history.replaceState ) {
-    window.history.replaceState( null, null, window.location.href );
-}
-async function alterarStatusErro(idErro, id_abastecimento){
-    const acao = 'alterar-status-erro'
-	const data = new FormData()
-	data.append('idErro', idErro)
-    data.append('id_abastecimento', id_abastecimento)
-    data.append('acao', acao)
-
-	const req = await fetch('diesel-control-1.1/assets/controllers/abastecimentoDataBaseAcess.php', {
-		method: 'POST',
-		body: data
-	})
-
-	const json = await req.json()
-
-	const idRow =`td${id_abastecimento}`
-
-	novoStatus = json.erro_status
-
-	if(novoStatus == 1){
-		document.getElementById(idRow).innerHTML='NÃO'
-		document.getElementById(idRow).className='w3-red'
-	}else{
-		document.getElementById(idRow).innerHTML='SIM'
-		document.getElementById(idRow).className='w3-green'
-	}
-}
 async function buscarInfoVeiculo(id){
 
 	const dados = await fetch(`diesel-control-1.1/assets/controllers/abastecimentoDataBaseAcess.php?&acao=ultimoKm&id=${id}`)
     const response = await dados.json()
-    console.log(response)
 	document.getElementById("setor").value = response['dados'].setor
 	document.getElementById("ultimoKmCad").value = response['dados'].ultimoKm
 	document.getElementById("ultimoHrCad").value = response['dados'].ultimoHr
