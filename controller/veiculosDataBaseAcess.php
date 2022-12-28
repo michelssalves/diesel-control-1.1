7ff5e5e2b02db83ba0ea5a94f7a497ea7a405aa3
Michel <michelssalves@gmail.com>
2022-12-28 11:00:02 -0300

    teste Ref: 28-12-2022 - 11:00 -> Michel

--- controller/veiculosDataBaseAcess.php
+++ controller/veiculosDataBaseAcess.php
@@ -73,7 +73,7 @@ function filtrarVeiculos($filtroPrefixo, $filtroCombustivel,$filtroMarca, $filtr
 
             $resultados = selectCountVeiculos();  
 
-            $number_pages = ceil($resultados / $result_for_page);
+            $number_pages = ceil(250 / $result_for_page);
             $max_link = 2;
     
             $txtTableVeiculos .= '<nav aria-label="Page navigation example"><ul class="pagination pagination-sm justify-content-center">';
