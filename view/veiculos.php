34a89e00d42dc4cbb79fcbe150a73fe3f6e208f5
Michel <michelssalves@gmail.com>
2022-12-28 11:20:49 -0300

    teste Ref: 28-12-2022 - 11:20 -> Michel

--- view/veiculos.php
+++ view/veiculos.php
@@ -31,11 +31,11 @@ include '../controller/veiculosDataBaseAcess.php';
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col-md-auto mt-4">
-                <form action="controle-de-veiculos" method="POST">
+                <form action="controle-de-veiculos-novo?page=1" method="POST">
                     <input type="hidden" name="page" value="1">
                     <?php $link = "PopupCenter('cadastrar-veiculo',' Cadastrar Veiculos',400,900)"; ?>
                     <button class="btn btn-success btn-sm" onclick="table2excel('t1')">Excel</button>
-                    <a href="controle-de-veiculos?page=1"><button name="page" value="1" type="submit" class='btn btn-warning btn-sm'>Filtrar</button></a>
+                  <button name="page" value="1" type="submit" class='btn btn-warning btn-sm'>Filtrar</button>
                     <button type="button" class='btn btn-primary btn-sm' data-bs-toggle='modal' data-bs-target='#modalCadastrarVeiculo'>Cadastrar</button>
                     <button name="acao" value="limpar" type="submit" class='btn btn-secondary btn-sm'>Limpar</button>
 
