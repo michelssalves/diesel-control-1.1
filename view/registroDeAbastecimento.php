2f1a44f06ae3e160bff506b6474f0a466efa65d7
Michel <michelssalves@gmail.com>
2022-12-28 13:03:35 -0300

    teste Ref: 28-12-2022 - 13:03 -> Michel

--- view/registroDeAbastecimento.php
+++ view/registroDeAbastecimento.php
@@ -1,5 +1,6 @@
 ﻿<?php
 session_start();
+include '../controller/config.php';
 include '../controller/checkAcess.php';
 include '../model/Abastecimentos.php';
 include '../controller/abastecimentoDataBaseAcess.php';
