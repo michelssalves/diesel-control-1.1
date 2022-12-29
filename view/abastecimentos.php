860dbcff438c6b625c893e334021012f93f78748
Michel <michelssalves@gmail.com>
2022-12-29 00:18:10 -0300

    teste Ref: 29-12-2022 - 00:18 -> Michel

--- view/abastecimentos.php
+++ view/abastecimentos.php
@@ -3,7 +3,7 @@ session_start();
 include '../model/Abastecimentos.php';
 include '../controller/abastecimentosController.php';
 
-echo $_SESSION['id_permissao']
+echo $_SESSION['id_permissao'];
 ?>
 <head>
     <meta charset="utf-8">
