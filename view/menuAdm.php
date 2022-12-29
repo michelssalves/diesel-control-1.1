57453d7e6932241416f869063fa0ef01ccb2228f
Michel <michelssalves@gmail.com>
2022-12-29 16:12:58 -0300

    teste Ref: 29-12-2022 - 16:12 -> Michel

--- view/menuAdm.php
+++ view/menuAdm.php
@@ -39,7 +39,7 @@
             </div>
         </div>
     </div>
-    <div class="container-fluid p-6">
+    <div class="container-fluid p-4">
         <div class="table-responsive">
             <table class="table table-sm fs-6 mb-0">
                 <thead class="fundo-cabecalho">
