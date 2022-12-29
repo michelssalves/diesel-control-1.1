e78a239974d8a8670fd03a650576966e41e5e616
Michel <michelssalves@gmail.com>
2022-12-29 11:42:26 -0300

    teste Ref: 29-12-2022 - 11:42 -> Michel

--- view/menuOperador.php
+++ view/menuOperador.php
@@ -22,10 +22,11 @@
         </div>
     </div>
     <div class="container">
-    <div class="fundo-botoes rounded">
+  
         <div class="row justify-content-md-center">
+        <div class="fundo-botoes rounded">
             <div class="col-md-auto mt-4">  
-            <
+            
             <form method="POST">  
                 <button type="button" class='btn btn-success btn-lg' data-bs-toggle='modal' data-bs-target='#modalCadastrarAbastecimento'>Cadastrar</button>
                 <a href="logout-novo" class="btn btn-danger btn-lg">Sair</a>
