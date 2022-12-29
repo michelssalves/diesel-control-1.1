3d6cfdea2ae1408b49b1dbc587daec75c0242db4
Michel <michelssalves@gmail.com>
2022-12-29 16:05:57 -0300

    tste Ref: 29-12-2022 - 16:05 -> Michel

--- view/header.php
+++ view/header.php
@@ -14,7 +14,6 @@
    <!--  /TABLESAW - DEIXA A TABELA RESPONSIVA-->
     <script src="diesel-control-1.1/assets/js/preloader.js"></script>
     <script src="diesel-control-1.1/assets/js/jquery-3.6.1.min.js"></script>
-    <script src="diesel-control-1.1/assets/js/sorttable.js"></script>
     <script src="diesel-control-1.1/assets/js/scripts.js"></script>
     <script src="diesel-control-1.1/assets/js/jquery.table2excel.js"></script>
     <script src="diesel-control-1.1/assets/js/fontawesome.all.min.js"></script>
