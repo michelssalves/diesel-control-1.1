17d3aa45f64b325f8aba859f8fe16a3fb50b35b0
Michel <michelssalves@gmail.com>
2022-12-29 10:14:41 -0300

    teset Ref: 29-12-2022 - 10:14 -> Michel

--- controller/validaToken.php
+++ controller/validaToken.php
@@ -1,15 +1,22 @@
 <?php
-if($_SESSION['id_funcionario']){
-    include '../model/Funcionarios.php';
+if((!isset($_SESSION['id_funcionario'])) && (!isset($_SESSION['usuario'])) &&  (!isset($_SESSION['nome'])) && (!isset($_SESSION['id_permissao'])) && (!isset($_SESSION['token']))){
+    session_start();
+    ob_start();
+    unset($_SESSION['id_funcionario'],$_SESSION['usuario'],$_SESSION['nome'],$_SESSION['id_permissao'],$_SESSION['token']);
+    header("Location: login-diesel-control-novo");
+}else{
+    if($_SESSION['id_funcionario']){
+        include '../model/Funcionarios.php';
 
-    $acao = $_REQUEST['acao'];
+        $acao = $_REQUEST['acao'];
 
-    $funcionario = verificaToken($_SESSION['id_funcionario']);
+        $funcionario = verificaToken($_SESSION['id_funcionario']);
 
-    if($_SESSION['token'] <> $funcionario[0]){
-        
-        session_destroy();
-        header("Location:  login-diesel-control-novo");
+        if($_SESSION['token'] <> $funcionario[0]){
+            
+            session_destroy();
+            header("Location:  login-diesel-control-novo");
+        }
     }
 }
 
