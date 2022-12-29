31d2a8eceae07fe43ea91d3a174e470c9b120aa3
Michel <michelssalves@gmail.com>
2022-12-29 09:45:02 -0300

    tete Ref: 29-12-2022 - 09:45 -> Michel

--- controller/logout.php
+++ controller/logout.php
@@ -8,11 +8,11 @@ if($_SESSION['id_funcionario']){
 
     var_dump($funcionario);
 
-    if($_SESSION['token'] <> $token){
+  /*  if($_SESSION['token'] <> $token){
         
         session_destroy();
         header("Location:  login-diesel-control-novo");
-    }
+    }*/
 
     if($acao == 'sair'){
         session_destroy();
