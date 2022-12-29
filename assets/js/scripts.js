ec8f70ecc1f2ed405eb3783989082397a511167f
Michel <michelssalves@gmail.com>
2022-12-29 13:59:00 -0300

    teste Ref: 29-12-2022 - 13:58 -> Michel

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -79,3 +79,9 @@ function impedirClicks(){
 	document.getElementById("loading").style.display = "block";
 	
 }
+
+$(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(10).fadeOut('slow'); 
+    $('body').delay(10).css({'overflow': 'visible'});
+  })
