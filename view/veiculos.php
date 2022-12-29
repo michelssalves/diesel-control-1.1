669eaea594c6763a22973efffe0bcc959c9e3ce8
Michel <michelssalves@gmail.com>
2022-12-29 10:53:10 -0300

    teste Ref: 29-12-2022 - 10:53 -> Michel

--- view/veiculos.php
+++ view/veiculos.php
@@ -44,9 +44,9 @@ include 'header.php';
         <a class="w3-bar-item w3-button w3-right"><?php // $usuario; ?></a>
     </div>-->
 
-    <div class="container mt-2 mb-1 h-25">
+    <div class="container mt-2 mb-1 ">
     <div style="background-color: #5F9EA0;">
-        <div class="row justify-content-md-center">
+        <div class="row justify-content-md-center h-25">
         
             <div class="col-md-auto mt-4">
             
