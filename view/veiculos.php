7afd1000dc3819c3dc41e41d347d9a6981e09c30
Michel <michelssalves@gmail.com>
2022-12-29 10:29:44 -0300

    teste Ref: 29-12-2022 - 10:29 -> Michel

--- view/veiculos.php
+++ view/veiculos.php
@@ -13,13 +13,13 @@ include 'header.php';
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item p-1">
+                    <li class="nav-item p-0">
                     <a href="controle-de-combustivel-novo" class="btn btn-secondary btn">Menu Principal</a>
                     </li>
-                    <li class="nav-item p-1">
+                    <li class="nav-item p-0">
                     <a href="controle-de-combustivel-novo" class="btn btn-secondary btn">Controles</a>
                     </li>
-                    <li class="nav-item p-1">
+                    <li class="nav-item p-0">
                         <a href="logout-novo" class="btn btn-warning btn">Sair</a>
                     </li>
                 </ul>
