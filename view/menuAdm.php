e44d0229f6267ee6f2f219183b2cdf331f38ca41
Michel <michelssalves@gmail.com>
2022-12-29 15:44:27 -0300

    teste Ref: 29-12-2022 - 15:44 -> Michel

--- view/menuAdm.php
+++ view/menuAdm.php
@@ -137,7 +137,7 @@
                 <table id="t1" data-tablesaw-sortable data-tablesaw-sortable-switch class="tablesaw table-sm fs-6 mb-0" data-tablesaw-mode="columntoggle" data-tablesaw-minimap>
                     <thead class="fundo-cabecalho">
                         <tr>
-                            <th data-tablesaw-sortable-col data-tablesaw-priority="1">
+                            <th data-tablesaw-sortable-col data-tablesaw-priority="3">
                                 <center>Data
                             </th>
                             <th data-tablesaw-sortable-col data-tablesaw-priority="0">
@@ -155,10 +155,10 @@
                             <th data-tablesaw-sortable-col data-tablesaw-priority="1">
                                 <center>Prefixo
                             </th>
-                            <th data-tablesaw-sortable-col data-tablesaw-priority="3">
+                            <th data-tablesaw-sortable-col data-tablesaw-priority="1">
                                 <center>Placa
                             </th>
-                            <th data-tablesaw-sortable-col data-tablesaw-priority="1">
+                            <th data-tablesaw-sortable-col data-tablesaw-priority="3">
                                 <center>Combustivel
                             </th>
                             <th data-tablesaw-sortable-col data-tablesaw-priority="0">
