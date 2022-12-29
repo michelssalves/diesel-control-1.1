cab21e2de121a374ec3ebb4cc4ef3ef545e23099
Michel <michelssalves@gmail.com>
2022-12-29 13:21:18 -0300

    teste Ref: 29-12-2022 - 13:21 -> Michel

--- view/abastecimentos.php
+++ view/abastecimentos.php
@@ -1,9 +1,45 @@
+
+<style>
+    #blanket,#aguarde {
+    position: fixed;
+    display: none;
+}
+
+#blanket {
+    left: 0;
+    top: 0;
+    background-color: #f0f0f0;
+    filter: alpha(opacity = 65);
+    height: 100%;
+    width: 100%;
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=65)";
+    opacity: 0.65;
+    z-index: 9998;
+}
+
+#aguarde {
+    width: auto;
+    height: 30px;
+    top: 40%;
+    left: 45%;
+    background: url('http://i.imgur.com/SpJvla7.gif') no-repeat 0 50%;
+    line-height: 30px;
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+    z-index: 9999;
+    padding-left: 27px;
+}
+</style>
+<script>
+    $(document).ready(function() {
+    $('.btn-theme').click(function(){
+        $('#aguarde, #blanket').css('display','block');
+    });
+});
+</script>
 <body>
-<body>
-<div id="loading" style="display: none" align="center">
-	<img src="diesel-control-1.1/assets/img/gifs/aguarde12.gif"/>
-</div>
-<div class="conteudo" id="conteudo" style="display: block">   
+<div id="blanket"></div>
+<div id="aguarde">Aguarde...</div> 
 
 <?php
 session_start();
